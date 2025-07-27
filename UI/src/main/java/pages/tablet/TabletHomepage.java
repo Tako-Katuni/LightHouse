83e2fd52118a691d4f2ff509777b626895e3a718
user2 <iraklizviadauri2003@gmail.com>
2025-07-27 16:45:39 +0400

    Revert "finished tablet selectors, configured gitignore"
    This reverts commit c996974bbdfb91054a2052ab09923a69eff16b36.

--- UI/src/main/java/pages/tablet/TabletHomepage.java
+++ UI/src/main/java/pages/tablet/TabletHomepage.java
@@ -14,7 +14,6 @@ public class TabletHomepage extends BasePage {
         headerLanguageChangeButton = page.locator("app-header tbcx-language-select");
         footerTermsAndFeesButton = page.locator("//*[text()=\"Personal\"]/ancestor::*/following-sibling::tbcx-pw-footer-sub-item//*[text()=\"Terms and Fees\"]");
 
-
     }
 }
 
